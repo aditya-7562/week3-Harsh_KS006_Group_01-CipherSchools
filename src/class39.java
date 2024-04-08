57a8a1e63c1b551ad87b0f66eb0001e12c726a66
aditya-7562 <aditya.12114424@lpu.in>
2024-04-08 17:19:33 +0530

    done with class 39 assignment

---
+++ src/class39.java
@@ -0,0 +1,58 @@
+public class class39 {
+
+    static boolean checkPalindrome(String s){
+        s = s.toLowerCase();
+        s = s.toUpperCase();
+        int l = 0;
+        int r = s.length() - 1;
+        while(l<r){
+            if(s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    static boolean isAnagram(String s1, String s2){
+
+        if(s1.length() != s2.length()) return false;
+
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        int n = s1.length();
+
+        int freqArr1[] = new int[26];
+        int freqArr2[] = new int[26];
+
+        for(int i = 0; i<n; i++){
+            int index1 = s1.charAt(i) - 'a';
+            freqArr1[index1]++;
+
+            int index2 = s2.charAt(i) - 'a';
+            freqArr2[index2]++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(freqArr1[i] != freqArr2[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "MADAM";
+        String s2 = "Cipher";
+        String s3 = "Madam";
+        System.out.println(checkPalindrome(s));
+        System.out.println(checkPalindrome(s2));
+        System.out.println(checkPalindrome(s3));
+
+        System.out.println(isAnagram("CAT", "TAC"));
+        System.out.println(isAnagram("CAT", "TAG"));
+        System.out.println(isAnagram("CAT", "TACC"));
+    }
+}
